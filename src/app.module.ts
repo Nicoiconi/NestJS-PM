301ccb16b2c470d52113e49f54729279c017d134
Nicolas <nicolas.iconicoff@gmail.com>
2024-03-06 16:57:48 -0300

    app module + main config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceConfig } from './config/data.source';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({ ...DataSourceConfig }),
+    UsersModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { CORS } from './constants/cors';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan("dev"))
+
+  // manejar Dto con class validator y validar la información en base a los controladores
+  app.useGlobalPipes(new ValidationPipe({
+    transformOptions: {
+      enableImplicitConversion: true
+    }
+  }))
+
+  const reflector = app.get(Reflector)
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector))
+
+  const configService = app.get(ConfigService)
+
+  app.enableCors(CORS)
+
+  app.setGlobalPrefix("api/v1")
+
+  const appPort = configService.get("APP_PORT")
+
+  await app.listen(appPort || 4000)
+
+  const appUrl = await app.getUrl()
+
+  console.log(`App running on: ${appUrl}`)
 }
 bootstrap();
