e2cd503771e1dd051c4a187cc26a7350298a19c0
Nicolas <nicolas.iconicoff@gmail.com>
2024-03-07 21:24:06 -0300

    app.module: change modules structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceConfig } from './config/data.source';
-import { BuyersModule } from './buyers/buyers.module';
-import { SellersModule } from './sellers/sellers.module';
+// import { BuyersModule } from './buyers/buyers.module';
+// import { SellersModule } from './sellers/sellers.module';
+// import { PostsModule } from './posts/posts.module';
+// import { BuyerMatchesModule } from './buyer-matches/buyer-matches.module';
+// import { SellerMatchesModule } from './seller-matches/seller-matches.module';
+import { MatchesModule } from './matches/matches.module';
 import { CategoriesModule } from './categories/categories.module';
-import { PostsModule } from './posts/posts.module';
+import { ClientsModule } from './clients/clients.module';
+import { BuyerPostsModule } from './buyer-posts/buyer-posts.module';
+import { SellerPostsModule } from './seller-posts/seller-posts.module';
+import { SalesModule } from './sales/sales.module';
+import { PurchasesModule } from './purchases/purchases.module';
 
 @Module({
   imports: [
@@ -16,10 +24,21 @@ import { PostsModule } from './posts/posts.module';
     }),
     TypeOrmModule.forRoot({ ...DataSourceConfig }),
     UsersModule,
-    BuyersModule,
-    SellersModule,
     CategoriesModule,
-    PostsModule
+    ClientsModule,
+    BuyerPostsModule,
+    SellerPostsModule,
+    MatchesModule,
+    SalesModule,
+    PurchasesModule,
+    
+    // BuyerMatchesModule,
+    // SellerMatchesModule,
+    // Client replace Buyers & Sellers
+    // BuyersModule,
+    // SellersModule,
+    // buyer & seller for each
+    // PostsModule,
   ],
   controllers: [],
   providers: [],
