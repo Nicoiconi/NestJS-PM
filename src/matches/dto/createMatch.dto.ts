792f0fc6e71392056966b4a459de265450bd1bf4
Nicolas <nicolas.iconicoff@gmail.com>
2024-03-11 14:40:32 -0300

    adjust imports

--- src/matches/dto/createMatch.dto.ts
+++ src/matches/dto/createMatch.dto.ts
@@ -1,8 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsNumberString, IsOptional, IsString } from "class-validator"
-import { BuyerPostsEntity } from "src/buyer-posts/entities/buyer-posts.entity"
-import { CategoriesEntity } from "src/categories/entities/categories.entity"
-import { SellerPostsEntity } from "src/seller-posts/entities/seller-posts.entity"
+import { BuyerPostsEntity } from "../../buyer-posts/entities/buyer-posts.entity"
+import { CategoriesEntity } from "../../categories/entities/categories.entity"
+import { SellerPostsEntity } from "../../seller-posts/entities/seller-posts.entity"
 
 export class CreateMatchDTO {
 
