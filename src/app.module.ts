f84c2ac8f465981e6a2b0eaf041e84a8dfe81c20
Nicolas <nicolas.iconicoff@gmail.com>
2024-03-12 10:07:46 -0300

    app.module: remove unnecessary code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,11 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceConfig } from './config/data.source';
-// import { BuyersModule } from './buyers/buyers.module';
-// import { SellersModule } from './sellers/sellers.module';
-// import { PostsModule } from './posts/posts.module';
-// import { BuyerMatchesModule } from './buyer-matches/buyer-matches.module';
-// import { SellerMatchesModule } from './seller-matches/seller-matches.module';
 import { MatchesModule } from './matches/matches.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ClientsModule } from './clients/clients.module';
 import { BuyerPostsModule } from './buyer-posts/buyer-posts.module';
 import { SellerPostsModule } from './seller-posts/seller-posts.module';
-import { SalesModule } from './sales/sales.module';
-import { PurchasesModule } from './purchases/purchases.module';
 import { CampaignsModule } from './campaigns/campaigns.module';
 
 @Module({
@@ -30,17 +23,7 @@ import { CampaignsModule } from './campaigns/campaigns.module';
     BuyerPostsModule,
     SellerPostsModule,
     MatchesModule,
-    SalesModule,
-    PurchasesModule,
-    CampaignsModule,
-    
-    // BuyerMatchesModule,
-    // SellerMatchesModule,
-    // Client replace Buyers & Sellers
-    // BuyersModule,
-    // SellersModule,
-    // buyer & seller for each
-    // PostsModule,
+    CampaignsModule
   ],
   controllers: [],
   providers: [],
