4f09733fe3035a78b2234f0c8dfc0054f99c46cb
Nicolas <nicolas.iconicoff@gmail.com>
2024-03-14 11:35:25 -0300

    remove unnecessary code + correct migration scripts

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -39,44 +39,9 @@ const DevDataSourceConfig: DataSourceOptions = {
   namingStrategy: new SnakeNamingStrategy(),
 }
 
-// export const DataSourceConfig: DataSourceOptions =
-//   process.env.NODE_ENV === "development"
-//     ? {
-//       type: 'postgres',
-//       host: configService.get("DEV_DB_HOST"),
-//       port: configService.get("DEV_DB_PORT"),
-//       username: configService.get("DEV_DB_USER"),
-//       password: configService.get("DEV_DB_PASSWORD"),
-//       database: configService.get("DEV_DB_NAME"),
-//       entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
-//       migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-//       synchronize: false,
-//       migrationsRun: true,
-//       logging: false,
-//       namingStrategy: new SnakeNamingStrategy(),
-//     }
-//     : {
-//       type: 'postgres',
-//       host: configService.get("TEST_DB_HOST"),
-//       port: configService.get("TEST_DB_PORT"),
-//       username: configService.get("TEST_DB_USER"),
-//       password: configService.get("TEST_DB_PASSWORD"),
-//       database: configService.get("TEST_DB_NAME"),
-//       entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
-//       migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-//       synchronize: false,
-//       migrationsRun: true,
-//       logging: false,
-//       namingStrategy: new SnakeNamingStrategy(),
-//     }
-
-console.log(process.env.NODE_ENV)
-
 export const DataSourceConfig: DataSourceOptions =
   process.env.NODE_ENV === "development"
     ? DevDataSourceConfig
     : TestDataSourceConfig
-
-console.log(DataSourceConfig)
 
 export const AppDS = new DataSource(DataSourceConfig)
